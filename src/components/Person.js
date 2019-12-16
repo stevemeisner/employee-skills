00fa3379911ddff5c18cc94a9afab73017b72c7d
Stephen Meisner <steve.meisner@anthroware.com>
2019-12-15 22:04:40 -0500

    removed old eslint comment

--- src/components/Person.js
+++ src/components/Person.js
@@ -1,6 +1,4 @@
-/* eslint-disable react/no-this-in-sfc */
-/* eslint-disable no-unused-vars */
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import Select from 'react-select';
 
